bb32a44da1f82d231f9395c8e9bc4021075f179c
MufazalAhmad <mufazal.149.id@gmail.com>
2023-02-23 15:57:51 +0500

    new commit save

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,20 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
           ],
         ),
       ),
