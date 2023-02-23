6fa99abb72dd0fe67ee366f9afdcf2c468182987
MufazalAhmad <mufazal.149.id@gmail.com>
2023-02-23 14:52:01 +0500

    login add .

---
+++ lib/screen/login.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Wolfiz"), actions: []),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            const Text("Hi, how are you"),
+          ],
+        ),
+      ),
+    );
+  }
+}
